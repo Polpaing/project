0ea096fdfea258481c2b3bfe073394b89babaff0
Polpaing <kizzednx@gmail.com>
2025-07-23 13:05:36 +0700

    Delete ProjectIT directory

--- ProjectIT/controls/fetchRoom.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include './controls/db.php';
-    session_start();
-
-    // ดึงข้อมูลผู้ใช้งานจาก Data base
-    $sql = "SELECT `id`, `name_room`, `description`, `building`, `created_at` FROM `room`";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-?>
--- ProjectIT/controls/fetchUser.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include './controls/db.php';
-    session_start();
-
-    // ดึงข้อมูลผู้ใช้งานจาก Data base
-    $sql = "SELECT `id`, `first_name`, `last_name`, `phone`, `email`, `password`, `created_at` FROM `users`";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-?>
